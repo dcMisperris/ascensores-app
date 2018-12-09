bda288ec324e671e387630bc8d548d32ee059c28
dcMisperris <paginawebduoc2018@gmail.com>
2018-12-09 15:03:30 -0400

    serializer

--- man_ascensores/settings.py
+++ man_ascensores/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
+    
     'rest_framework',
     'apps.perfiles',
 ]
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'man_ascensores.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
